607c19b39b8dc9fba7fdc1f0f02403282184d782
Christophe Bismuth <christophe.bismuth@gmail.com>
2020-03-23 00:32:18 +0100

    Add CMake support

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "config.h"
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    std::cout << argv[0] << " Version " << puzzles_VERSION << std::endl;
+  }
+
+  return 0;
+}
