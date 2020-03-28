b916308f08a0e175242460a7795bd4919c79ba14
Christophe Bismuth <christophe.bismuth@gmail.com>
2020-03-28 22:38:46 +0100

    Add code coverage support

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 ![CI](https://github.com/cbismuth/puzzles/workflows/CI/badge.svg)
 
-Puzzles written in C/C++.
+This repository contains puzzles written in the C/C++ programming language.
 
 ## Build
 
-To build project execute `build.sh` script.
+This project uses the CMake build system without advanced configuration and a convenience [build.sh](build.sh) script is available.
+
+This script executes `clang-format` to format C/C++ source files according to this [.clang-format](.clang-format) configuration file.
+
+Debug build step generates code coverage reports at `cmake-build-debug/coverage/index.html`.
 
 ## Issues
 
@@ -22,5 +26,6 @@ Written by [Christophe Bismuth](https://www.linkedin.com/in/cbismuth/), licensed
 
 ## References
 
-* [Modern CMake Template](https://github.com/rvarago/modern-cmake-template) by [Rafael Varago](https://github.com/rvarago)
-* [C++ project setup with CMake & unit tests (google test)](https://raymii.org/s/tutorials/Cpp_project_setup_with_cmake_and_unit_tests.html) by [Remy van Elst](https://raymii.org/s/static/About.html)
+* [Modern CMake Template](https://github.com/rvarago/modern-cmake-template) by [Rafael Varago](https://github.com/rvarago).
+* [C++ project setup with CMake & unit tests (google test)](https://raymii.org/s/tutorials/Cpp_project_setup_with_cmake_and_unit_tests.html) by [Remy van Elst](https://raymii.org/s/static/About.html).
+* Proteus `clang-format` [configuration files](https://gitlab.cern.ch/proteus/proteus/-/commit/8d906a45801c03832531e243f41f5f5a83177de0).
--- build.sh
+++ build.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# Clear terminal screen
 clear -x
 
-# Install prerequisites on Debian / Ubuntu systems
+# Install prerequisites on Debian based systems only
 if [[ -f /etc/lsb-release ]]; then
-  sudo apt-get -y install build-essential cmake clang-format git
+  sudo apt-get update
+  sudo apt-get -y install --no-install-recommends ca-certificates git cmake make clang-format gcc g++ lcov
 fi
 
-# Install Google Test dependency
+# Set Google Test dependency path
 DIR_VENDORS_GTEST="vendors/google/googletest"
 
+# Clone Google Test repository if not exists
 if [[ ! -d  "${DIR_VENDORS_GTEST}" ]]; then
   mkdir -p "${DIR_VENDORS_GTEST}"
   git clone https://github.com/google/googletest.git "${DIR_VENDORS_GTEST}"
 fi
 
+# Clean Google Test working copy and checkout tag
 cd "${DIR_VENDORS_GTEST}" || exit 1
 git reset --hard HEAD
 git clean -xffd
@@ -23,14 +29,47 @@ git pull
 git -c advice.detachedHead=false checkout release-1.10.0
 cd - || exit 1
 
-# Build project
+# Set project debug output path
 CMAKE_OUTPUT_DEBUG="cmake-build-debug"
 
+# Clean project debug output path
 rm    -rf "${CMAKE_OUTPUT_DEBUG}"
 mkdir -p  "${CMAKE_OUTPUT_DEBUG}"
 
+# Change directory to project debug output path
 cd "${CMAKE_OUTPUT_DEBUG}" || exit 1
-cmake ..
-make clean format all test
+
+# Build project with debug options
+cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..
+cmake --build . --config Debug -- -j "$(nproc)"
+
+# Run tests
+ctest -j "$(nproc)" --output-on-failure
+
+# Run application
 app/puzzles
+
+# Generate coverage reports
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info '*gtest*' '/usr/*' --output-file coverage.info.cleaned
+genhtml -o coverage coverage.info.cleaned
+
+# Change directory back to previous one
+cd - || exit 1
+
+# Set project release output path
+CMAKE_OUTPUT_RELEASE="cmake-build-release"
+
+# Clean project release output path
+rm    -rf "${CMAKE_OUTPUT_RELEASE}"
+mkdir -p  "${CMAKE_OUTPUT_RELEASE}"
+
+# Change directory to project release output path
+cd "${CMAKE_OUTPUT_RELEASE}" || exit 1
+
+# Build project with release options
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release -- -j "$(nproc)"
+
+# Change directory back to previous one
 cd - || exit 1
